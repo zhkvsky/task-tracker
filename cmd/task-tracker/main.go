7f56d48223731704a71179e7094c1bf6354c12b3
Kirill Zhykovsky <zhykovsky1@icloud.com>
2025-09-22 11:05:03 +0400

    Function for adding tasks is now working

--- cmd/task-tracker/main.go
+++ cmd/task-tracker/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/zhkvsky/task-tracker/internal/task"
+	"os"
+)
 
 func main() {
-	fmt.Println("task tracker is running")
+	fmt.Println("Task tracker 1.0")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide a task name. Type \"Help\" for more information.")
+		return
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "add":
+		if len(os.Args) < 3 {
+			fmt.Println("Please provide a description for the task")
+			return
+		}
+		description := os.Args[2]
+		newTask, err := task.AddTask("tasks.json", description)
+		if err != nil {
+			fmt.Println("Error adding task:", err)
+			return
+		}
+		fmt.Printf("Task added successfully: (ID: %d)\n", newTask.ID)
+	case "list":
+		fmt.Println("Listing all tasks:")
+
+	case "update":
+		fmt.Println("Updating task:")
+
+	case "delete":
+		fmt.Println("Deleting task:")
+
+	case "mark-in-progress":
+		fmt.Println("Marking task an in progress:")
+
+	case "mark-done":
+		fmt.Println("Marking task as done:")
+
+	default:
+		fmt.Println("Unknown command")
+	}
 }
--- internal/task/task.go
+++ internal/task/task.go
@@ -1,1 +1,80 @@
 package task
+
+import (
+	"encoding/json"
+	"os"
+	"time"
+)
+
+type TaskStatus string
+
+const (
+	TaskStatusTodo       TaskStatus = "todo"
+	TaskStatusInProgress TaskStatus = "in-progress"
+	TaskStatusDone       TaskStatus = "done"
+)
+
+type Task struct {
+	ID          int        `json:"id"`
+	Description string     `json:"description"`
+	Status      TaskStatus `json:"status"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+}
+
+func LoadTask(filename string) ([]Task, error) {
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		return []Task{}, nil
+	}
+
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	tasks := []Task{}
+	if err := json.Unmarshal(data, &tasks); err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func SaveTasks(filename string, tasks []Task) error {
+	data, err := json.Marshal(tasks)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(filename, data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func AddTask(filename string, description string) (Task, error) {
+	tasks, err := LoadTask(filename)
+	if err != nil {
+		return Task{}, err
+	}
+
+	newID := len(tasks) + 1
+	newTask := Task{
+		ID:          newID,
+		Description: description,
+		Status:      TaskStatusTodo,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	tasks = append(tasks, newTask)
+
+	err = SaveTasks(filename, tasks)
+	if err != nil {
+		return newTask, err
+	}
+
+	return newTask, err
+}
