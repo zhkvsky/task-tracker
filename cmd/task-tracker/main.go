a81b98764b455fcedb1e4a59489a11fe9b1c32c9
Kirill Zhykovsky <zhykovsky1@icloud.com>
2025-10-02 22:54:47 +0400

    Added: task listing, task deletion, task updating

--- cmd/task-tracker/main.go
+++ cmd/task-tracker/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/zhkvsky/task-tracker/internal/task"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -17,6 +19,7 @@ func main() {
 	command := os.Args[1]
 
 	switch command {
+
 	case "add":
 		if len(os.Args) < 3 {
 			fmt.Println("Please provide a description for the task")
@@ -29,14 +32,64 @@ func main() {
 			return
 		}
 		fmt.Printf("Task added successfully: (ID: %d)\n", newTask.ID)
+
 	case "list":
-		fmt.Println("Listing all tasks:")
+		tasks, err := task.ListTasks("tasks.json")
+		if err != nil {
+			fmt.Println("Error listing tasks:", err)
+			return
+		}
+
+		if len(tasks) == 0 {
+			fmt.Println("No tasks found.")
+			return
+		}
+
+		for _, t := range tasks {
+			fmt.Printf("[%d] %s (%s)\n", t.ID, t.Description, t.Status)
+		}
 
 	case "update":
-		fmt.Println("Updating task:")
+		if len(os.Args) < 4 {
+			fmt.Println("Please provide a id and description for the task")
+			return
+		}
+
+		id, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Println("Invalid id:", os.Args[2])
+			return
+		}
+
+		description := strings.Join(os.Args[3:], " ")
+
+		updatedTask, err := task.UpdateTask("tasks.json", id, description)
+		if err != nil {
+			fmt.Println("Error updating task:", err)
+			return
+		}
+
+		fmt.Printf("Task updated successfully: (ID: %d)\n", updatedTask.ID)
 
 	case "delete":
-		fmt.Println("Deleting task:")
+		if len(os.Args) < 3 {
+			fmt.Println("Please provide an id of the task")
+			return
+		}
+
+		id, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Println("Invalid id:", os.Args[2])
+			return
+		}
+
+		deletedTask, err := task.DeleteTask("tasks.json", id)
+		if err != nil {
+			fmt.Println("Error deleting task:", err)
+			return
+		}
+
+		fmt.Printf("Task deleted successfully: (ID: %d, %s)\n", deletedTask.ID, deletedTask.Description)
 
 	case "mark-in-progress":
 		fmt.Println("Marking task an in progress:")
